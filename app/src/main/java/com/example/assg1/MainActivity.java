b8e0b1a0bf74420f6232d2f8f8141fbea0d54409
amr <amr20012200131@hotmail.com>
2023-05-06 18:40:42 +0300

    version 1

---
+++ app/src/main/java/com/example/assg1/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.assg1;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.assg1.respiratory_system.RespiratorySystem;
+
+public class MainActivity extends AppCompatActivity {
+    //    EditText NoseText;
+    boolean NoseCheck;
+    boolean MouthCheck;
+    boolean TracheaCheck;
+    boolean LungsCheck;
+    boolean DiaphragmCheck;
+
+
+    @SuppressLint({"ResourceType", "SetTextI18n"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RespiratorySystem Rsystem = new RespiratorySystem();
+        EditText NoseText = findViewById(R.id.NoseText);
+        EditText MouthText = findViewById(R.id.MouthText);
+        EditText TracheaText = findViewById(R.id.TracheaText);
+        EditText LungsText = findViewById(R.id.LungsText);
+        EditText DiaphragmText = findViewById(R.id.DiaphragmText);
+        Button NoseButton = findViewById(R.id.NoseButton);
+        Button MouthButton = findViewById(R.id.MouthButton);
+        Button TracheaButton = findViewById(R.id.TracheaButton);
+        Button LungsButton = findViewById(R.id.LungsButton);
+        Button DiaphragmButton = findViewById(R.id.DiaphragmButton);
+        TextView hintLabel = findViewById(R.id.hintLabel);
+        TextView explainLabel = findViewById(R.id.explainLabel);
+        Button check = findViewById(R.id.check);
+        Button rest = findViewById(R.id.rest);
+
+        check.setOnClickListener(v -> {
+            NoseCheck = NoseText.getText().toString().equals("Nose") || NoseText.getText().toString().equals("nose");
+
+            MouthCheck = MouthText.getText().toString().equals("Mouth") || MouthText.getText().toString().equals("mouth");
+
+            TracheaCheck = TracheaText.getText().toString().equals("Trachea") || TracheaText.getText().toString().equals("trachea");
+
+            LungsCheck = LungsText.getText().toString().equals("Lungs") || LungsText.getText().toString().equals("lungs");
+
+            DiaphragmCheck = DiaphragmText.getText().toString().equals("Diaphragm") || DiaphragmText.getText().toString().equals("diaphragm");
+
+            if (NoseCheck && MouthCheck && TracheaCheck && LungsCheck && DiaphragmCheck) {
+                NoseText.setVisibility(View.GONE);
+                MouthText.setVisibility(View.GONE);
+                TracheaText.setVisibility(View.GONE);
+                LungsText.setVisibility(View.GONE);
+                DiaphragmText.setVisibility(View.GONE);
+
+                NoseButton.setVisibility(View.VISIBLE);
+                MouthButton.setVisibility(View.VISIBLE);
+                TracheaButton.setVisibility(View.VISIBLE);
+                LungsButton.setVisibility(View.VISIBLE);
+                DiaphragmButton.setVisibility(View.VISIBLE);
+                explainLabel.setVisibility(View.VISIBLE);
+                check.setVisibility(View.GONE);
+                rest.setVisibility(View.GONE);
+                hintLabel.setText("Select the part you want to learn about");
+            }
+            if (!NoseCheck) {
+                NoseText.setBackgroundResource(R.drawable.border_error);
+                hintLabel.setText("Make sure you enter the right parts name");
+            }
+
+            if (!MouthCheck) {
+                MouthText.setBackgroundResource(R.drawable.border_error);
+                hintLabel.setText("Make sure you enter the right parts name");
+            }
+
+            if (!TracheaCheck) {
+                TracheaText.setBackgroundResource(R.drawable.border_error);
+                hintLabel.setText("Make sure you enter the right parts name");
+            }
+            if (!LungsCheck) {
+                LungsText.setBackgroundResource(R.drawable.border_error);
+                hintLabel.setText("Make sure you enter the right parts name");
+            }
+
+            if (!DiaphragmCheck) {
+                DiaphragmText.setBackgroundResource(R.drawable.border_error);
+                hintLabel.setText("Make sure you enter the right parts name");
+            }
+        });
+
+        rest.setOnClickListener(v -> {
+            NoseText.setText("");
+            MouthText.setText("");
+            TracheaText.setText("");
+            LungsText.setText("");
+            DiaphragmText.setText("");
+        });
+
+        NoseButton.setOnClickListener(v -> explainLabel.setText(Rsystem.getNose()));
+        MouthButton.setOnClickListener(v -> explainLabel.setText(Rsystem.getMouth()));
+        TracheaButton.setOnClickListener(v -> explainLabel.setText(Rsystem.getTrachea()));
+        LungsButton.setOnClickListener(v -> explainLabel.setText(Rsystem.getLungs()));
+        DiaphragmButton.setOnClickListener(v -> explainLabel.setText(Rsystem.getDiaphragm()));
+    }
+}
---
+++ app/src/main/java/com/example/assg1/respiratory_system/RespiratorySystem.java
@@ -0,0 +1,30 @@
+package com.example.assg1.respiratory_system;
+
+public class RespiratorySystem {
+    String Nose;
+    String Mouth;
+    String Trachea;
+    String Lungs;
+    String Diaphragm;
+
+    public String getNose() {
+        return Nose = "The nose is the part of our body that helps us breathe. When we inhale, air flows through our nostrils and into the nasal cavity, where it is filtered by tiny hairs and mucus. The nose also warms and adds humidity to the air, and helps us smell different scents.";
+    }
+
+    public String getMouth() {
+        return Mouth ="The mouth is the part of our body that we use for eating, drinking, and talking. We use our teeth and tongue to chew food and move it around in our mouth, and our lips to drink liquids. We also use our mouth to make different sounds when we speak.";
+    }
+
+    public String getTrachea() {
+        return Trachea ="The trachea is like a tube that connects our nose and mouth to our lungs. When we breathe in, air travels down the trachea, which has tiny hairs and mucus that help to clean the air. The trachea then splits into two smaller tubes that lead to our lungs. When we breathe out, air flows back up through the same tube and out of our nose or mouth.";
+    }
+
+    public String getLungs() {
+        return Lungs ="The lungs are two important organs in our chest that help us breathe. They take in air, which has oxygen that our body needs, and then release air, which has carbon dioxide that our body doesn't need. The lungs are protected by a thin layer and are helped by a large muscle in our chest.";
+    }
+
+    public String getDiaphragm() {
+        return Diaphragm = "The diaphragm is a large muscle in our chest that helps us breathe. When we inhale, it moves down and creates space for air to come in. When we exhale, it moves up and pushes air out.";
+    }
+
+}
